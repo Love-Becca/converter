e1b9da73aeaef8ef43574e7614403de8ea173ccc
P4ND4 <63761684+Love-Becca@users.noreply.github.com>
2023-02-22 19:13:32 +0100

    add: header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './ConverterHeader';
+import Body from './Converter';
 
 function App() {
   return (
     <div className="App">
-      <h1>we are testing</h1>
+      <Header />
+      <Body />
     </div>
   );
 }
---
+++ src/ConverterHeader.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './Converter.css'
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <nav>
+                    <ul>
+                        <li>Download</li>
+                        <li>About</li>
+                        <li>Sign in</li>
+                        <li>Sign up</li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+}
+ 
+export default Header;
