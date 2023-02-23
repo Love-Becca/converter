3d70b79d6af86b799b524f4fdb1b8621e69f6eae
P4ND4 <63761684+Love-Becca@users.noreply.github.com>
2023-02-23 16:09:56 +0100

    CI: choose files

--- src/Converter.js
+++ src/Converter.js
@@ -1,20 +1,38 @@
 import React from "react";
+import Steps from "./steps";
 import './Converter.css'
 import cloud from './assets/space.png'
+import file from './assets/file.svg'
 
 const Body = () => {
+    const handleClick = (e)=>{
+        const files = e.target.files;
+    }
     return ( 
         <>
             <div className="hero-body">
-                <div className="hero-info">
-                    <h2 className="hero-title">File Converter</h2>
-                    <p className="hero-details">To get started use the button below and select files to convert from your laptop.</p>
-                    <input type="file"/>
+                <div className="info-group">
+                    <div className="hero-info">
+                        <h2 className="hero-title">File Converter</h2>
+                        <p className="hero-details">To get started use the button below and select files to convert from your laptop.</p>
+                    </div>
+                    <div className="hero-steps">
+                        <Steps />
+                    </div>
+                    <label className="button" htmlFor="upload">Choose File</label>
+                    <input type="file" id="upload" onChange={handleClick}/>
+                    {/* <template>
+                        <div className="file-attached">
+                            <img src={file} alt="file" />
+                            <p>{files}</p>
+                        </div>
+                    </template> */}
                 </div>
                 <div className="hero-image">
                     <img src={cloud} alt="file" className="cloud-image"/>
                 </div>
             </div>
+            
         </>
     );
 }
---
+++ src/steps.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Converter.css'
+
+const Steps = () => {
+    return (  
+        <div className='step-body'>
+            <div className='step-1'>
+                <div>1</div>
+                <p>choose file</p>
+            </div>
+            <span></span>
+            <div className='step-2'>
+                <div>2</div>
+                <p>convert</p>
+            </div>
+            <span></span>
+            <div className='step-3'>
+                <div>3</div>
+                <p>Download pdf</p>
+            </div>
+        </div>
+    );
+}
+ 
+export default Steps;
