fc29c8e5f6ed21184d67e8e6326dc2a0ac05f8fc
P4ND4 <63761684+Love-Becca@users.noreply.github.com>
2023-02-28 00:40:30 +0100

    CI: conversion of image to pdf

--- src/Converter.js
+++ src/Converter.js
@@ -1,46 +1,63 @@
-import React,{useState, useEffect, useCallback} from "react";
+import React,{useState, useEffect, useCallback, useMemo} from "react";
 import Steps from "./steps";
 import './Converter.css'
 import cloud from './assets/hero-image.png'
 import Processing from "./Processing";
 import convert from'./assets/convert.png'
-// import pdfkit from 'pdfkit'
-// import fs from 'fs'
+import jsPDF from 'jspdf';
 
 
 const Body = () => {
-    // const docs = new pdfkit();
-    // docs.pipe(fs.createWriteStream('output.pdf'));
-    // console.log(docs);
     const fileError = "Maximum file reached";
-    const [data, setData] = useState(new Set());
-    const [files, setFiles] = useState([]);
-    const [loading, setLoading] = useState({
+    const [data, setData] = useState(new Set()); // FROM ITERATION
+    const [files, setFiles] = useState([]); //DATA FROM INPUT
+    const [values, setValues] = useState([])
+    const [loading, setLoading] = useState({    //HANDLE LOADING
         isLoading: false
     });
 
-    const toggle = useCallback(()=>{
+    const toggle = useCallback((x)=>{    //loading feature
         setLoading(prevLoading=>{
             return{
                 ...prevLoading,
                 isLoading:!loading.isLoading
             }
         })
-
     },[loading.isLoading])
 
-    const handleClick = (e)=>{
-        if (loading.isLoading === false){
-            setFiles(prevFiles =>{
-                return[
-                    ...prevFiles,
-                    e.target.files
-                ]
-            });
-        }       
-    }
 
-    useEffect(() => {
+    const handleChange = (e)=>{  //get files from input
+        if (loading.isLoading === false){
+            setFiles(prevFiles =>[...prevFiles, e.files]);
+            setValues(prevValues =>[...prevValues, e.value])
+        };
+               
+    }
+    const handleFiles = useCallback(() => {
+        const doc = new jsPDF();
+        const fileNames = [];
+        files.map((y)=>{
+            Object.values(y).forEach(item=>{
+                const reader = new FileReader();
+                reader.onload = (e)=>{
+                    const url = e.target.result
+                    const fileName = item.name.replace(/\.[^/.]+$/, '');
+                    const pageWidth = doc.internal.pageSize.getWidth();
+                    const imgProps = doc.getImageProperties(url);
+                    const pdfHeight = (imgProps.height * pageWidth) / imgProps.width;
+                    doc.addPage();
+                    doc.addImage(url, 'JPEG', 10, 20, pageWidth - 20, pdfHeight - 20, null, 'FAST');
+                   // fileNames.push(fileName);
+                   doc.save(`${fileName}.pdf`); //CURRENTLY SAVING ONE PICTURE
+                };
+                reader.readAsDataURL(item);
+               
+            })
+        })
+        
+    }, [files]);
+
+    useEffect(() => {   //iterate over multi-dimensional data gotten from input
         if (files.length < 4) {
             for (const i of files) {
                 for (const j of i) {
@@ -50,9 +67,7 @@ const Body = () => {
         }
     }, [files]);
 
-    
-
-    const saved = Array.from(data).map(x=>
+    const saved = Array.from(data).map(x=> // render data TO FILE
     <Processing
         key={x.size}
         id={x.size}
@@ -70,7 +85,7 @@ const Body = () => {
                     <Steps />
                 </div>
                 <label className="button" htmlFor="upload">Choose File</label>
-                <input type="file" id="upload" onChange={handleClick}/>
+                <input type="file" id="upload" multiple={false} accept=".jpg,.png,.xlsx,.doc,.docx,.xml,application/msword" onChange={(e)=>handleChange(e.target)}/>
                 <p className="error-message">{files.length > 3 && fileError}</p>
                 <div className="file-list">
                     <div className="files">
@@ -79,7 +94,7 @@ const Body = () => {
                     <div className={files.length !== 0 && loading.isLoading?"loader": "hide"}></div>
                     <div className="convert-icon">
                         {files.length !== 0 && !loading.isLoading && (
-                            <img src={convert} alt="convert-file" onClick={toggle} className="show" />
+                            <img src={convert} alt="convert-file" onClick={()=>{toggle();handleFiles()}} className="show" />
                         )}
                     </div>
                 </div>
